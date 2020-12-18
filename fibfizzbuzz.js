ade3ec41209bd3fcbc2ea3844f7580f13c0e071e
Vaughn McPherson <Vaughnsmcp@gmail.com>
2020-12-18 03:27:50 -0800

    FizzleIzzleBuzzle

--- fibfizzbuzz.js
+++ fibfizzbuzz.js
@@ -1,31 +1,25 @@
-function FizzBuzz(value1, value2,) {
+function FizzBuzz(value1, value2) {
   let returnValue = "";
-  let f0=1;
-  let f1=1;
-  
+  let f0 = 1;
+  let f1 = 1;
+
   for (let i = 1; i <= 100; i++) {
     if (i % value1 == 0 && i % value2 == 0) {
-      returnValue += "FizzBuzz ";}
-   
-    else if ( i == f1){
-      returnValue += "Fib ";
-
-    }
-    else if (i % value1 == 0) {
+      returnValue += "FizzBuzz ";
+    } else if (i == f1) {
+      returnValue += "FibleIzzleBuzzle  ";
+    } else if (i % value1 == 0) {
       returnValue += "Fizz ";
     } else if (i % value2 == 0) {
       returnValue += "Buzz ";
     } else {
       returnValue += i + " ";
     }
-    if (i >= f1){
+    if (i >= f1) {
       var v = f0 + f1;
-      f0=f1
-      f1=v
-
-
+      f0 = f1;
+      f1 = v;
     }
-    
   }
   return returnValue;
 }
@@ -36,9 +30,8 @@ function Fibbonacci() {
   for (let i = 2; i <= 100; i++) {
     var v = f0 + f1;
     returnValue += v + " ";
-    f0=f1
-    f1=v
-
+    f0 = f1;
+    f1 = v;
   }
   return returnValue;
 }
@@ -48,7 +41,6 @@ function buzzIt() {
   let val1 = document.getElementById("Fizzvalue").value;
   let val2 = document.getElementById("Buzzvalue").value;
   output = FizzBuzz(val1, val2);
-  // output = Fibbonacci();
 
   document.getElementById("results").innerHTML = output;
 }
