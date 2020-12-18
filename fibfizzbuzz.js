54d43901e3d512bdac44e226a0fa8b1037088774
Vaughn McPherson <Vaughnsmcp@gmail.com>
2020-12-17 19:06:08 -0800

    Fizz and Buzz and Fibs

--- fibfizzbuzz.js
+++ fibfizzbuzz.js
@@ -1,15 +1,31 @@
-function FizzBuzz(value1, value2) {
+function FizzBuzz(value1, value2,) {
   let returnValue = "";
+  let f0=1;
+  let f1=1;
+  
   for (let i = 1; i <= 100; i++) {
     if (i % value1 == 0 && i % value2 == 0) {
-      returnValue += "FizzBuzz ";
-    } else if (i % value1 == 0) {
+      returnValue += "FizzBuzz ";}
+   
+    else if ( i == f1){
+      returnValue += "Fib ";
+
+    }
+    else if (i % value1 == 0) {
       returnValue += "Fizz ";
     } else if (i % value2 == 0) {
       returnValue += "Buzz ";
     } else {
       returnValue += i + " ";
     }
+    if (i >= f1){
+      var v = f0 + f1;
+      f0=f1
+      f1=v
+
+
+    }
+    
   }
   return returnValue;
 }
@@ -31,8 +47,8 @@ function buzzIt() {
   let output = "";
   let val1 = document.getElementById("Fizzvalue").value;
   let val2 = document.getElementById("Buzzvalue").value;
-  // output = FizzBuzz(val1, val2);
-  output = Fibbonacci();
+  output = FizzBuzz(val1, val2);
+  // output = Fibbonacci();
 
   document.getElementById("results").innerHTML = output;
 }
